9baf6684bd610bafdb2cfd006c44efd50d9dd510
Philipp von dem Bussche <philipp.bussche@gmail.com>
2017-05-11 20:42:20 +0200

    added data sinking into ES. Also made the filtering for real tweets arriving from the endpoint a bit nicer. added an example properties file what the job needs to run properly

--- src/main/java/de/bussche/flink/StreamingJob.java
+++ src/main/java/de/bussche/flink/StreamingJob.java
@@ -18,48 +18,93 @@ package de.bussche.flink;
  * limitations under the License.
  */
 
+import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.RuntimeContext;
+import org.apache.flink.shaded.com.google.common.collect.Maps;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.flink.util.Collector;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.flink.streaming.connectors.elasticsearch.ElasticsearchSink;
+import org.apache.flink.streaming.connectors.elasticsearch.IndexRequestBuilder;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.Requests;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.transport.TransportAddress;
 
 public class StreamingJob {
 
 	public static void main(String[] args) throws Exception {
-		// set up the streaming execution environment
+
+		// get the execution environment ready.
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
+		// read the properties file. Make sure a file called settings.properties is in the resources directory
+		// you can have a look at settings.properties.template for how the structure should look like
 		final TwitterProperties properties = new TwitterProperties();
 
+		// add the twitter source. This twitter source uses the filtered status endpoint which accepts to filter for certain user ids
+		// one would have to find those user ids out in order to configure them here
 		DataStream<String> streamSource = env.addSource(new TwitterSource(properties.getProperties()));
 
-		DataStream<String> textStream = streamSource.map(new MapFunction<String, String>() {
-			private transient ObjectMapper jsonParser;
-
-			@Override
-			public String map(String tweet) throws Exception {
-				String text = "n/a";
-				if (jsonParser == null) {
-					jsonParser = new ObjectMapper();
-				}
-				try {
-					JsonNode jsonNode = jsonParser.readValue(tweet, JsonNode.class);
-					boolean hasText = jsonNode.has("text");
-					if (hasText) {
-						text = jsonNode.get("text").asText();
+		// filter the stream for all messages from twitter which are not empty and contain text
+		// map stream data to a new string which represents the text property of the json arriving from twitter
+		DataStream<String> textStream = streamSource
+				.filter(new FilterFunction<String>() {
+					@Override
+					public boolean filter(String s) {
+						return !s.isEmpty() && s.contains("text");
 					}
-				} catch (Exception e) {
+				})
+				.map(new MapFunction<String, String>() {
+					private transient ObjectMapper jsonParser;
+					@Override
+					public String map(String tweet) throws Exception {
+						String text = "";
+						if (jsonParser == null) {
+							jsonParser = new ObjectMapper();
+						}
+						JsonNode jsonNode = jsonParser.readValue(tweet, JsonNode.class);
+						boolean hasText = jsonNode.has("text");
+						if (hasText) {
+							text = jsonNode.get("text").asText();
+						}
+						return text;
+					}
+				});
 
-				}
-				return text;
-			}
-		});
-
+		// print the data arrving from twitter out on the console
 		textStream.print();
 
+		// let's configure the ElasticSearch node where the mapped data should be stored in
+		Map<String, String> esConfig = Maps.newHashMap();
+		esConfig.put("bulk.flush.max.actions", "1");
+		esConfig.put("cluster.name", "flink-twitter-es");
+		List<TransportAddress> transports = new ArrayList<>();
+		transports.add(new InetSocketTransportAddress("1.1.1.1", 9300));
+
+		// add the ElasticSearch node as sink for Flink to store the mapped data in
+		// we would be looking for an index called 'twitter-index' and the documents will be of type 'twitter-doc'
+		// the document to be stored will have one property called 'text' with a value from the twitter json
+		textStream.addSink(new ElasticsearchSink<>(esConfig, transports, new IndexRequestBuilder<String>() {
+			@Override
+			public IndexRequest createIndexRequest(String s, RuntimeContext ctx) {
+				return Requests.indexRequest()
+						.index("twitter-index")
+						.type("twitter-doc")
+						.source("text", s)
+						.opType("index");
+			}
+		}));
+
 		// execute program
-		env.execute("Flink Streaming Java API Skeleton");
+		env.execute("Flink Streaming Java API Twitter to ES example");
 	}
+
 }
